83c6a7a4a5a81b838fa833fdf61bac5d05306e3c
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-05-09 00:05:09 -0600

    Update Barrel.java
    Add Logger

--- src/main/java/org/barrelmc/barrel/Barrel.java
+++ src/main/java/org/barrelmc/barrel/Barrel.java
@@ -13,14 +13,15 @@ package org.barrelmc.barrel;
 
 import org.barrelmc.barrel.network.converter.BlockConverter;
 import org.barrelmc.barrel.server.ProxyServer;
+import org.barrelmc.barrel.utils.Logger;
 
 public class Barrel {
 
     public static String DATA_PATH = System.getProperty("user.dir") + "/";
 
     public static void main(String[] args) {
-        System.out.println("Starting Barrel Proxy software");
+        System.out.println("Starting Barrel Proxy software CREA Edition");
         BlockConverter.init();
-        new ProxyServer(DATA_PATH);
+        new ProxyServer(DATA_PATH, new Logger("§6BarrelMC"));
     }
 }
