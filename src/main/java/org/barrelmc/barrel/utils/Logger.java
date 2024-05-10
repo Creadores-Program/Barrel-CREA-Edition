8d7e79dbaa3f2f9c2bb7d5ccc1949fc0c4fe7ae9
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-05-09 19:23:36 -0600

    Update Logger.java

--- src/main/java/org/barrelmc/barrel/utils/Logger.java
+++ src/main/java/org/barrelmc/barrel/utils/Logger.java
@@ -12,76 +12,76 @@ public class Logger{
     return this.prefix;
   }
   public void emergency(String message){
-    System.out.println(TextFormat.colorize('§', "[§cEMERGENCY§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.RED.getAnsiCode()+"EMERGENCY"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
   }
   public void emergency(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', "[§cEMERGENCY§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.RED.getAnsiCode()+"EMERGENCY"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
     er.printStackTrace();
   }
   public void info(String message){
-    System.out.println(TextFormat.colorize('§', "[§aINFO§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.GREEN.getAnsiCode()+"INFO"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
   }
   public void info(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', "[§aINFO§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.GREEN.getAnsiCode()+"INFO"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
     er.printStackTrace();
   }
   public void alert(String message){
-    System.out.println(TextFormat.colorize('§', "[§eWARN§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.YELLOW.getAnsiCode()+"WARN"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
   }
   public void alert(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', "[§eWARN§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.YELLOW.getAnsiCode()+"WARN"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
     er.printStackTrace();
   }
   public void critical(String message){
-    System.out.println(TextFormat.colorize('§', "[§4FATAL§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("[§4FATAL"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
   }
   public void critical(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', "[§4FATAL§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("[§4FATAL"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
     er.printStackTrace();
   }
   public void error(String message){
-    System.out.println(TextFormat.colorize('§', "[§cERROR§r] ["+ this.prefix +"§r] "+message));
+    System.out.printIn("["++"ERROR"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
   }
   public void error(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', "[§cERROR§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["++"ERROR"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
     er.printStackTrace();
   }
   public void warning(String message){
-    System.out.println(TextFormat.colorize('§', "[§eWARN§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.YELLOW.getAnsiCode()+"WARN"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message));
   }
   public void warning(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', "[§eWARN§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.YELLOW.getAnsiCode()+"WARN"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
     er.printStackTrace();
   }
   public void notice(String message){
-    System.out.println(TextFormat.colorize('§', "[§eWARN§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.YELLOW.getAnsiCode()+"WARN"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
   }
   public void notice(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', "[§eWARN§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.YELLOW.getAnsiCode()+"WARN"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
     er.printStackTrace();
   }
   public void debug(String message){
-    System.out.println(TextFormat.colorize('§', "[DEBUG] ["+ this.prefix +"§r] "+message));
+    System.out.println("[DEBUG] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
   }
   public void debug(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', "[DEBUG] ["+ this.prefix +"§r] "+message));
+    System.out.println("[DEBUG] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
     er.printStackTrace();
   }
   public void logException(Throwable er){
     er.printStackTrace();
   }
   public void log(String message){
-    System.out.println(TextFormat.colorize('§', message));
+    System.out.println(message);
   }
   public void log(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', message));
+    System.out.println(message);
     er.printStackTrace();
   }
   public void warn(String message){
-    System.out.println(TextFormat.colorize('§', "[§eWARN§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.YELLOW.getAnsiCode()+"WARN"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
   }
   public void warn(String message, Throwable er){
-    System.out.println(TextFormat.colorize('§', "[§eWARN§r] ["+ this.prefix +"§r] "+message));
+    System.out.println("["+TextFormat.YELLOW.getAnsiCode()+"WARN"+TextFormat.RESET.getAnsiCode()+"] ["+ this.prefix +TextFormat.RESET.getAnsiCode()+"] "+message);
     er.printStackTrace();
   }
 }
