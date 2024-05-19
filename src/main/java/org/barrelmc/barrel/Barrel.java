7ef409032ae04f71ecd01f30729584ae2f2cde48
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-05-18 22:50:41 -0600

    Update Barrel.java

--- src/main/java/org/barrelmc/barrel/Barrel.java
+++ src/main/java/org/barrelmc/barrel/Barrel.java
@@ -20,6 +20,8 @@ public class Barrel {
 
     public static void main(String[] args) {
         System.out.println("Starting Barrel Proxy CREA Edition software");
+        System.setProperty("java.compiler", "javac");
+        System.getProperties().putIfAbsent("io.netty.allocator.type", "unpooled");
         System.out.println("Barrel CREA Edition is distributed under the MIT License");
         BlockConverter.init();
         new ProxyServer(DATA_PATH);
