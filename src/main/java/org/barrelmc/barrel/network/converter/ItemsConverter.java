68226b12fc553efaba45a2f07853aa42711ddd27
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-05-19 23:24:15 -0600

    Create ItemsConverter.java

---
+++ src/main/java/org/barrelmc/barrel/network/converter/ItemsConverter.java
@@ -0,0 +1,20 @@
+package org.barrelmc.barrel.network.converter;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.barrelmc.barrel.utils.FileManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ItemsConverter {
+  public static void init() {
+    
+  }
+  public static String BedrockToJava(){
+    
+  }
+  public static String JavaToBedrock(){
+    
+  }
+}
