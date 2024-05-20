797cdb3e623e0f6af59a18b0c5391d19950a0b77
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-05-20 08:20:58 -0600

    Update ItemsConverter.java

--- src/main/java/org/barrelmc/barrel/network/converter/ItemsConverter.java
+++ src/main/java/org/barrelmc/barrel/network/converter/ItemsConverter.java
@@ -8,13 +8,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ItemsConverter {
+  public static final HashMap<String, String> ItemsJEtoBE = new HashMap<>();
+  public static final HashMap<String, String> ItemsBEtoJE = new HashMap<>();
   public static void init() {
-    
+    JsonObject jsonObject = FileManager.getJsonObjectFromResource("runtime_items.json");
+
+    assert jsonObject != null;
+
+    for(Map.Entry<String, JsonElement> entry : jsonObject.entrySet()){
+      String JavaID = entry.getKey();
+      JsonObject itemEntry = entry.getValue().getAsJsonObject();
+      ItemsJEtoBE.put(JavaID, itemEntry.get("name").getAsString() + "::"+itemEntry.get("damage").getAsString());
+      ItemsBEtoJE.put(itemEntry.get("name").getAsString() + "::"+itemEntry.get("damage").getAsString(), JavaID);
+    }
   }
   public static String BedrockToJava(String IDName){
-    
+    return ItemsBEtoJE.getOrDefault(IDName, "minecraft:air");
   }
   public static String JavaToBedrock(String IDName){
-    
+    return ItemsJEtoBE.getOrDefault(IDName, "minecraft:air::0");
   }
 }
