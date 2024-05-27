493b4f46630953a6d2288f79900fc10136630357
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-05-27 01:34:57 -0600

    Create ChatCommandPacket.java

---
+++ src/main/java/org/barrelmc/barrel/network/translator/java/ChatCommandPacket.java
@@ -0,0 +1,19 @@
+package org.barrelmc.barrel.network.translator.java;
+import com.github.steveice10.mc.protocol.codec.MinecraftPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.serverbound.ServerboundChatCommandPacket;
+import org.barrelmc.barrel.network.translator.interfaces.JavaPacketTranslator;
+import org.barrelmc.barrel.player.Player;
+import org.barrelmc.barrel.server.ProxyServer;
+import org.cloudburstmc.protocol.bedrock.packet.CommandRequestPacket;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandOriginType;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandOriginData;
+public class ChatCommandPacket implements JavaPacketTranslator {
+  @Override
+  public void translate(MinecraftPacket pk, Player player) {
+    ServerboundChatCommandPacket packet = (ServerboundChatCommandPacket) pk;
+    CommandRequestPacket Crp = new CommandRequestPacket();
+    Crp.setVersion(ProxyServer.getInstance().getBedrockPacketCodec().getProtocolVersion());
+    Crp.setCommand(packet.getCommand());
+    player.getBedrockClientSession().sendPacket(Crp);
+  }
+}
