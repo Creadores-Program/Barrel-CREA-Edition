07b6743340d55658f89635a346b7331effc98ba5
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-05-27 01:13:52 -0600

    Create ChatCommandPacket.java

---
+++ src/main/java/org/barrelmc/barrel/network/translator/java/ChatCommandPacket.java
@@ -0,0 +1,23 @@
+package org.barrelmc.barrel.network.translator.java;
+import com.github.steveice10.mc.protocol.codec.MinecraftPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.serverbound.ServerboundChatCommandPacket;
+import org.barrelmc.barrel.network.translator.interfaces.JavaPacketTranslator;
+import org.barrelmc.barrel.player.Player;
+import org.barrelmc.barrel.server.ProxyServer;
+import org.cloudburstmc.protocol.bedrock.packet.CommandRequestPacket;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandOriginType;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandOriginData;
+public class ChatCommandPacket implements JavaPacketTranslator {
+  @Override
+  public void translate(MinecraftPacket pk, Player player) {
+    ServerboundChatCommandPacket packet = (ServerboundChatCommandPacket) pk;
+    CommandRequestPacket Crp = new CommandRequestPacket();
+    Crp.setVersion(ProxyServer.getInstance().getBedrockPacketCodec().getProtocolVersion());
+    Crp.setCommand(packet.getCommand());
+    CommandOriginData Cod = new CommandOriginData();
+    Cod.setOrigin(CommandOriginType.PLAYER);
+    Cod.setUuid(java.util.UUID.fromString(player.getUUID()));
+    Crp.setCommandOriginData(Cod);
+    player.getBedrockClientSession().sendPacket(Crp);
+  }
+}
