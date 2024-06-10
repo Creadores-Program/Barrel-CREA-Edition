70eb37bbbebb460fc8f23a1c8b176bad75e2a44e
Creadores Program <cpcreador@gmail.com>
2024-06-10 12:26:52 -0600

    Update TextPacket.java

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/TextPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/TextPacket.java
@@ -24,7 +24,10 @@ public class TextPacket implements BedrockPacketTranslator {
                 break;
             }
             case TRANSLATION: {
-                
+                if(!player.getTraslateAd()){
+                    player.setTraslateAd(true);
+                    player.sendMessage("§cChat translation not implemented! Force language on Minecraft bedrock server or use Translator for Geyser Reverse Barrel for Nukkit");
+                }
             }
             default: {
                 player.sendMessage(packet.getMessage());
