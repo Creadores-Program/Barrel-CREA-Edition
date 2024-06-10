4d3e484c9891372e4879cbdcd617fb7ce4967b75
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-06-10 08:12:20 -0600

    Update Player.java

--- src/main/java/org/barrelmc/barrel/player/Player.java
+++ src/main/java/org/barrelmc/barrel/player/Player.java
@@ -10,6 +10,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.github.steveice10.mc.protocol.data.game.entity.object.Direction;
 import com.github.steveice10.mc.protocol.packet.ingame.clientbound.ClientboundSystemChatPacket;
 import com.github.steveice10.mc.protocol.packet.ingame.clientbound.level.ClientboundSetChunkCacheCenterPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.clientbound.level.ClientboundSetChunkCacheRadiusPacket;
 import com.github.steveice10.mc.protocol.packet.login.serverbound.ServerboundHelloPacket;
 import com.github.steveice10.packetlib.Session;
 import io.netty.bootstrap.Bootstrap;
@@ -145,6 +146,10 @@ public class Player extends Vector3 {
     @Getter
     @Setter
     private String language = "en-US";
+
+    @Getter
+    @Setter
+    private int distanceRender = 16;
 
     public Player(ServerboundHelloPacket loginPacket, Session javaSession) {
         this.packetTranslatorManager = new PacketTranslatorManager(this);
@@ -436,6 +441,7 @@ public class Player extends Vector3 {
     public void setPosition(Vector3f vector3f) {
         if (this.getFloorX() >> 4 != vector3f.getFloorX() >> 4 || this.getFloorZ() >> 4 != vector3f.getFloorZ() >> 4) {
             this.javaSession.send(new ClientboundSetChunkCacheCenterPacket(vector3f.getFloorX() >> 4, vector3f.getFloorZ() >> 4));
+            this.javaSession.send(new ClientboundSetChunkCacheRadiusPacket(this.distanceRender));
         }
         super.setPosition(vector3f);
     }
@@ -444,6 +450,7 @@ public class Player extends Vector3 {
     public void setPosition(double x, double y, double z) {
         if (this.getFloorX() >> 4 != (int) x >> 4 || this.getFloorZ() >> 4 != (int) z >> 4) {
             this.javaSession.send(new ClientboundSetChunkCacheCenterPacket((int) x >> 4, (int) z >> 4));
+            this.javaSession.send(new ClientboundSetChunkCacheRadiusPacket(this.distanceRender));
         }
         super.setPosition(x, y, z);
     }
