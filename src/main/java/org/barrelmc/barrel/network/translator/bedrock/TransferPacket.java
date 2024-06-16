17ed247eb44b7c0fe3cb70d7734e67e86aa7a0e0
Creadores Program <cpcreador@gmail.com>
2024-06-15 18:52:00 -0600

    Update TransferPacket.java

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/TransferPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/TransferPacket.java
@@ -9,12 +9,14 @@ public class TransferPacket implements BedrockPacketTranslator{
     org.cloudburstmc.protocol.bedrock.packet.TransferPacket packet = (org.cloudburstmc.protocol.bedrock.packet.TransferPacket) pk;
     if(ProxyServer.getInstance().getConfig().getAuth() == "offline"){
       try{
+        player.getChannel().close();
         player.offlineLogin(player.getHelloPacketJava(), packet.getAddress(), packet.getPort());
       }catch(Exception exception){
         player.getJavaSession().disconnect("Failed to transfer: " + exception);
       }
     }else{
       try{
+        player.getChannel().close();
         player.onlineLogin(player.getHelloPacketJava(), packet.getAddress(), packet.getPort());
       }catch(Exception exception){
         player.getJavaSession().disconnect("Failed to transfer: " + exception);
